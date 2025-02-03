05a134a61ed586f44016cc1e193ff8984eb92c7e
Jashwitha Balam <jashwithabalam4@gmail.com>
2025-02-03 14:49:05 +0530

    Update Race.cpp

--- Race.cpp
+++ Race.cpp
@@ -206,9 +206,7 @@ int main()
        
     do{  
         system("cls");  
-        gotoxy(10,5); cout<<" -------------------------- ";   
-        gotoxy(10,6); cout<<" |  Car Game by AZFAR       | ";   
-        gotoxy(10,7); cout<<" --------------------------";  
+
         gotoxy(10,9); cout<<"1. Start Game";  
         gotoxy(10,10); cout<<"2. Instructions";      
         gotoxy(10,11); cout<<"3. Quit";  
